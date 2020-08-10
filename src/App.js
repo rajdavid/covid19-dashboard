e319c21b65e3b8acfea6b2faa086bf8c29e86bb3
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-11 03:29:48 +0530

    added exception handling

--- src/App.js
+++ src/App.js
@@ -25,27 +25,29 @@ class App extends Component {
     let mergeData = []
     let i = 0;
     let j = 0;
-    console.log('len state data', this.state.data.length)
-    console.log('len testing  data',this.state.testingData.length)
-    for (i = 0; i < this.state.data.length; i++){
-      console.log('len state data',i)
-      for (j = 0; j < this.state.testingData.length; j++) { 
-        console.log('testing state data',j)
-        if (this.state.data[i].state === this.state.testingData[j].state) {
-          let obj = {}
-          console.log('dada')
-          obj.state = this.state.data[i].state
-          obj.confirmed = this.state.data[i].confirmed
-          obj.active = this.state.data[i].active
-          obj.recovered = this.state.data[i].recovered
-          obj.deaths = this.state.data[i].deaths
-          obj.testspermillion = this.state.testingData[j].testspermillion
-          obj.numicubeds = this.state.testingData[j].numicubeds
-          mergeData.push(obj);
+    //console.log('len state data', this.state.data.length)
+    //console.log('len testing  data',this.state.testingData.length)
+    if (this.state.data !== undefined && this.state.testingData !== undefined) {
+      for (i = 0; i < this.state.data.length; i++) {
+        console.log('len state data', i)
+        for (j = 0; j < this.state.testingData.length; j++) {
+          console.log('testing state data', j)
+          if (this.state.data[i].state === this.state.testingData[j].state) {
+            let obj = {}
+            console.log('dada')
+            obj.state = this.state.data[i].state
+            obj.confirmed = this.state.data[i].confirmed
+            obj.active = this.state.data[i].active
+            obj.recovered = this.state.data[i].recovered
+            obj.deaths = this.state.data[i].deaths
+            obj.testspermillion = this.state.testingData[j].testspermillion
+            obj.numicubeds = this.state.testingData[j].numicubeds
+            mergeData.push(obj);
+          }
         }
-      }
       
-    }
+      }
+    } 
     console.log('merged data',mergeData)
     this.setState({mergedData:mergeData})
   }
@@ -53,7 +55,9 @@ class App extends Component {
     let self=this;
     axios.get('https://api.covid19india.org/state_test_data.json')
         .then(function (response) {
+          console.log(response)
           let tData = response.data.states_tested_data;
+          
           self.setState({ testingData: tData.filter(obj => obj.updatedon === "08/08/2020") },
             () => {
             self.mergeData()
@@ -61,7 +65,7 @@ class App extends Component {
           
         })
     .catch(function (error) {
-        console.log(error);
+        console.log("from error ",error);
     })
     .finally(function () {
 
@@ -80,7 +84,10 @@ class App extends Component {
           () => {
           this.getTestingData()
         })
-      });
+      })
+      .catch(function (error) {
+            console.log("error from main api" , error);
+         })
 
       //var self=this
     // axios.get('https://api.covid19india.org/data.json')
