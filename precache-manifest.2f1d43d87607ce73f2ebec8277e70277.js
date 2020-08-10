9ddf1ce531eed32556875c650c2f3ca2e3ac75f1
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-11 02:37:07 +0530

    Updates

---
+++ precache-manifest.2f1d43d87607ce73f2ebec8277e70277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fd16ece0246651c5bb9c8e334b452d",
+    "url": "/covid19-dashboard/index.html"
+  },
+  {
+    "revision": "c0d8e11baafa9891dd71",
+    "url": "/covid19-dashboard/static/css/main.24755e1c.chunk.css"
+  },
+  {
+    "revision": "46b6600385568311e6b6",
+    "url": "/covid19-dashboard/static/js/2.0c13b1d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid19-dashboard/static/js/2.0c13b1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d8e11baafa9891dd71",
+    "url": "/covid19-dashboard/static/js/main.935755cb.chunk.js"
+  },
+  {
+    "revision": "36e3dd313605d9d377af",
+    "url": "/covid19-dashboard/static/js/runtime-main.8f9d1019.js"
+  }
+]);
