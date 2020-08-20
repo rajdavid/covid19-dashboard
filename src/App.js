2c79981b3fb1ab44f5f944064a48b00e7edc013c
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-20 16:22:44 +0530

    removed checks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import MetricsCard from './components/CaseMetrics';
 import Grid from '@material-ui/core/Grid';
 import DataTable from './components/MetricsTable';
 import axios from 'axios';
-//import { shadows } from '@material-ui/system';
 
 class App extends Component {
 
@@ -16,8 +15,7 @@ class App extends Component {
       testingData: [],
       mergedData: [],
       testedData:[],
-  }
-  //this.patientDataApi=this.patientDataApi.bind(this)
+      }
   }
   componentDidMount() {
     this.getPatientData()
@@ -28,16 +26,16 @@ class App extends Component {
     let mergeData = []
     let i = 0;
     let j = 0;
-    //console.log('len state data', this.state.data.length)
-    //console.log('len testing  data',this.state.testingData.length)
+    
     if (this.state.data !== undefined && this.state.testingData !== undefined) {
+      
       for (i = 0; i < this.state.data.length; i++) {
-        //console.log('len state data', i)
+        
         for (j = 0; j < this.state.testingData.length; j++) {
-          //console.log('testing state data', j)
+        
           if (this.state.data[i].state === this.state.testingData[j].state) {
             let obj = {}
-            //console.log('dada')
+        
             obj.state = this.state.data[i].state
             obj.confirmed = this.state.data[i].confirmed
             obj.active = this.state.data[i].active
@@ -48,26 +46,22 @@ class App extends Component {
             mergeData.push(obj);
           }
         }
-      
       }
     } 
-    //console.log('merged data',mergeData)
-    this.setState({mergedData:mergeData})
+            this.setState({ mergedData: mergeData })
   }
-    getTestingData = () => { 
+    
+  
+  getTestingData = () => { 
     let self=this;
     axios.get('https://api.covid19india.org/state_test_data.json')
         .then(function (response) {
-          //console.log(response)
+          
           let tData = response.data.states_tested_data;
-          if (tData !== undefined &&
-            response !== undefined &&
-            response.data.sresponse.data.states_tested_data > 0) {
             self.setState({ testingData: tData.filter(obj => obj.updatedon === "08/08/2020") },
               () => {
                 self.mergeData()
               })
-          }
         })
     .catch(function (error) {
         console.log("from error ",error);
@@ -75,69 +69,48 @@ class App extends Component {
     .finally(function () {
     });
   }
-  getPatientData =() =>  {
-    
-    /**
-     * added fetch for getting data from api
-    */
-    // fetch('https://api.covid19india.org/data.json')
-    //   .then(response => {
-    //     response.json()
-    //   })
-    //   .then(data => {
-    //     this.setState({ data: data.statewise },
-    //       () => {
-    //       this.getTestingData()
-    //     })
-    //   })
-    //   .catch(function (error) {
-    //         console.log("error from main api" , error);
-    //      })
-
+  
+  
+  getPatientData = () => {
     var self=this
     axios.get('https://api.covid19india.org/data.json')
       .then(response => {
-        if (response !== undefined  && response.data.statewise.length>0) {
+        
           self.setState({ data: response.data.statewise }, () => {
             self.getTestingData()
+          
           })
-        }
     })
     .catch(function (error) {
-        //console.log(error);
+        
     })
     .finally(function () {
     });
   }
+ 
+     getWholeCountryTestingData = () => { 
+     
+       let td = this
+      
+       axios.get('https://api.covid19india.org/data.json')
+            .then(function (response) {
+                  
+              let tData = response.data.tested;
+              tData = tData[tData.length - 1]
+              td.setState({ testedData: tData })
 
-  /*
-    Getting testing data for whole country 
-  */
-   getWholeCountryTestingData =()=>{ 
-     let td = this
-     axios.get('https://api.covid19india.org/data.json')
-     .then(function (response) {
-       if (response !== undefined &&
-        response.data.tested.length>0) {
-         //console.log('from whole country data',response)
-         let tData = response.data.tested;
-         tData = tData[tData.length - 1]
-         //console.log("testted Data", tData);
-         //console.log("length of tested data",tData.length)
-         td.setState({ testedData: tData })
-       }
      })
       .catch(function (error) {
-          console.log(error);
-       })
+        })
       .finally(function () {
 
       });
-      //console.log('after filtering data', this.testedData);
-    }
+      
+     }
 render() {
     return (
       <div className="App">
+        
         <AppHeader />
       
         <br />
